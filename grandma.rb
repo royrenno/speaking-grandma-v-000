4572a32c49d7b6c40eb5545b2c533ebc4ed6aec5
roy renno <royrenno@yahoo.com>
2019-01-12 23:47:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phase)
-if phrase=="I LOVE YOU GRANDMA!"
-elsif "I LOVE YOU TOO PUMPKIN"
 
-end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
