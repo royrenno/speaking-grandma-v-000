0c71962cdcfcb92bbf7b2ccf5d8341caca6936dc
roy renno <royrenno@yahoo.com>
2019-01-13 00:28:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phase)
   if phase== "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase== phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    return "HUH?!SPEAK UP SONNY!"
   else phrase== phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
     end
